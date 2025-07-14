668552e54d3f10f83b087bded858f3a46d0ee161
Sravan Kumar <sravan2418@outlook.com>
2025-07-13 21:34:08 -0500

    Eda

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,7 @@
 import sys 
 
-import logging
+from src.logger import logging
+
 
 def error_mesasge_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info() # which file, whcih line the exception has occured
@@ -19,3 +20,9 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_mesage
     
+if __name__=="__main__":
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Divide by zero")
+        raise CustomException(e,sys)
